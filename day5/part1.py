581a320ce64b7fcf15bf6d4e9a88fdc534e61334
David Morton <55235981+davidmorton0@users.noreply.github.com>
2024-12-06 08:58:41 +0000

    d6 p2

--- day5/part1.py
+++ day5/part1.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
-with open('input.txt', 'r') as file:
+from copy import deepcopy
+
+with open('example.txt', 'r') as file:
     input = file.read().split()
 grid = [list(line) for line in input]
 
@@ -68,19 +70,89 @@ def do_guard_turn(icon, guard_x, guard_y, grid):
         grid[guard_y][guard_x] = 'X'
     return grid
 
+def do_guard_turn_with_checking(icon, guard_x, guard_y, grid):
+    match icon:
+      case '^': #face up
+        new_x = guard_x
+        new_y = guard_y - 1
+      case '>':  # face right
+        new_x = guard_x + 1
+        new_y = guard_y
+      case 'v': #face down
+        new_x = guard_x
+        new_y = guard_y + 1
+      case '<':  # face left
+        new_x = guard_x - 1
+        new_y = guard_y
+
+    new_grid = False
+    if new_x < width and new_x >= 0 and new_y < height and new_y >= 0:
+        if grid[new_y][new_x] in ['.']:
+          move_guard(icon, guard_x, guard_y, grid)
+          new_grid = True
+        elif grid[new_y][new_x] in ['X']:
+          move_guard(icon, guard_x, guard_y, grid)
+        elif grid[new_y][new_x] == '#':
+          turn_guard(icon, guard_x, guard_y, grid)
+    else:
+        grid[guard_y][guard_x] = 'X'
+    return grid, new_grid
 
 def print_grid(grid):
     for line in grid:
         print(''.join(line))
 
-for n in list(range(0, 200000)):
+def run_guard(grid):
     icon, guard_y, guard_x = calculate_guard_position(grid)
-    if icon == '*':
-      print_grid(grid)
-      print('finished')
-      print(sum([len([pos for pos in line if pos == 'X']) for line in grid]))
+    while icon != '*':
+        grid = do_guard_turn(icon, guard_x, guard_y, grid)
+        icon, guard_y, guard_x = calculate_guard_position(grid)
+    return grid.copy()
 
-      break
-    grid = do_guard_turn(icon, guard_x, guard_y, grid)
+def check_grid_equality(grid1, grid2):
+    grid1_joined = ''.join([''.join(line) for line in grid1])
+    grid2_joined = ''.join([''.join(line) for line in grid2])
+    return grid1_joined == grid2_joined
 
+def run_guard_with_checking(grid):
+    icon, guard_y, guard_x = calculate_guard_position(grid)
+    old_grids = []
+    while icon != '*':
+        grid, check = do_guard_turn_with_checking(icon, guard_x, guard_y, grid)
+        icon, guard_y, guard_x = calculate_guard_position(grid)
+        if not check:
+            old_grids.append(deepcopy(grid))
+    return grid.copy()
+
+def generate_new_grids(grid_with_xs, initial_grid):
+    grids_to_check = []
+    for y, line in enumerate(grid_with_xs):
+        for x, pos in enumerate(line):
+            if pos == 'X':
+                new_grid = deepcopy(initial_grid)
+                new_grid[y][x] = '#'
+                grids_to_check.append(new_grid)
+    return grids_to_check
+
+initial_grid = deepcopy(grid)
+grid_with_xs = run_guard(grid)
+print(sum([len([pos for pos in line if pos == 'X']) for line in grid_with_xs]))
+
+grids_to_check = generate_new_grids(grid_with_xs, initial_grid)
+
+def check_grids():
+    for grid_to_check in grids_to_check:
+
+
+
+# for n in list(range(0, 200000)):
+#     icon, guard_y, guard_x = calculate_guard_position(grid)
+#     if icon == '*':
+#       print_grid(grid)
+#       print('finished')
+#       print(sum([len([pos for pos in line if pos == 'X']) for line in grid]))
+#
+#       break
+#     grid = do_guard_turn(icon, guard_x, guard_y, grid)
+#
 
