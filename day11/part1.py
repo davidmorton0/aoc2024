3c85ec59daedcf997b19e09cc572c89f2c96449a
David Morton <david.morton@fundingcircle.com>
2024-12-11 12:27:25 +0000

    d11-2

--- day11/part1.py
+++ day11/part1.py
@@ -1,5 +1,28 @@
 #!/usr/bin/env python
+from collections import defaultdict
 
 with open('input.txt', 'r') as file:
-    input = file.read().split("\n")
+    stones = defaultdict(lambda: 0)
+    for stone in [int(stone) for stone in file.read().split("\n")[0].split(' ')]:
+        stones[stone] += 1
 
+def blink(stones):
+    new_stones = defaultdict(lambda: 0)
+    for stone, number in stones.items():
+        if stone == 0:
+            new_stones[1] += number
+        elif len(str(stone)) % 2 == 0:
+            str_stone = str(stone)
+            mid_position = int(len(str_stone) / 2)
+            new_stones[(int(str_stone[0:mid_position]))] += number
+            new_stones[(int(str_stone[mid_position:]))] += number
+        else:
+            new_stones[(int(stone * 2024))] += number
+    return new_stones
+
+print(blink(stones))
+
+for n in range(75):
+    stones = blink(stones)
+
+print(sum(stones.values()))
