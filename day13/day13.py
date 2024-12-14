d3bfc22e96be8a9f06dd70925e6664b9ba19cc74
David Morton <55235981+davidmorton0@users.noreply.github.com>
2024-12-14 07:43:39 +0000

    day 13-2

--- day13/day13.py
+++ day13/day13.py
@@ -2,21 +2,31 @@
 import re
 from itertools import batched
 
+BUTTON_A_REGEX = r'Button A: X([+-]\d+), Y([+-]\d+)'
+BUTTON_B_REGEX = r'Button B: X([+-]\d+), Y([+-]\d+)'
+PRIZE_REGEX = r'Prize: X=(\d+), Y=(\d+)'
+
 with open('input.txt', 'r') as file:
     lines = [line for line in file.read().split("\n")]
 
+def get_values(regex, line):
+    match = re.match(regex, line)
+    return {"x": int(match[1]), "y": int(match[2])}
+
 machines = []
-for machine in batched(lines, 4):
-    match = re.match(r'Button A: X([+-]\d+), Y([+-]\d+)', machine[0])
-    button_a = {"x": int(match[1]), "y": int(match[2])}
-    match = re.match(r'Button B: X([+-]\d+), Y([+-]\d+)', machine[1])
-    button_b = {"x": int(match[1]), "y": int(match[2])}
-    match = re.match(r'Prize: X=(\d+), Y=(\d+)', machine[2])
-    prize = {"x": int(match[1]), "y": int(match[2])}
-    machines.append({"button_a": button_a, "button_b": button_b, "prize": prize})
+for machine_lines in batched(lines, 4):
+    machines.append({
+        "button_a": get_values(BUTTON_A_REGEX, machine_lines[0]),
+        "button_b": get_values(BUTTON_B_REGEX, machine_lines[1]),
+        "prize": get_values(PRIZE_REGEX, machine_lines[2])
+    })
 
 def check_value(a_pushes, a, b_pushes, b, prize):
-    return a_pushes * a["x"] + b_pushes * b["x"] == prize["x"] and a_pushes * a["y"] + b_pushes * b["y"] == prize["y"]
+    return a_pushes * a + b_pushes * b == prize
+
+def check_values(a_pushes, a, b_pushes, b, prize):
+    return (check_value(a_pushes, a["x"], b_pushes, b["x"], prize["x"]) and
+            check_value(a_pushes, a["y"], b_pushes, b["y"], prize["y"]))
 
 def calculate_tokens(machine):
     m_1 = machine["button_b"]["x"] * machine["button_a"]["y"]
@@ -33,7 +43,7 @@ def calculate_tokens(machine):
 
     a_pushes = int((t_1 - t_2) / (m_1 - m_2))
     b_pushes = int((t_3 - t_4) / (m_3 - m_4))
-    if check_value(a_pushes, machine["button_a"], b_pushes, machine["button_b"], machine["prize"]):
+    if check_values(a_pushes, machine["button_a"], b_pushes, machine["button_b"], machine["prize"]):
         return (3 * a_pushes + b_pushes)
     else:
         return 0
