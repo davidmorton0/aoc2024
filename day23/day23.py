0fc42c785052a397895be3126f49ab235b0d94b7
David Morton <55235981+davidmorton0@users.noreply.github.com>
2024-12-23 12:11:17 +0000

    day 23 refactor

--- day23/day23.py
+++ day23/day23.py
@@ -2,32 +2,66 @@
 from collections import defaultdict
 from itertools import combinations
 import re
+import time
 
-INPUT = 1
-FILENAME = ['example.txt', 'input_h.txt', 'input_w.txt'][INPUT]
-REGEX = r'(\w\w)-(\w\w)'
+start_time = time.time()
+
+INPUT = 2
+FILENAME = ['example.txt', 'example_2.txt', 'input_h.txt', 'input_w.txt'][INPUT]
+REGEX = r'(\w{2})-(\w{2})'
 
 with open(FILENAME, 'r') as file:
-    connections = [line for line in file.read().split("\n") if line != ""]
+    connection_list = [line for line in file.read().split("\n") if line != ""]
 
+def find_connections_by_computer(connection_list):
+    connections = defaultdict(lambda : [])
+    for connection in connection_list:
+        match =  re.match(REGEX, connection)
+        computer1 = match[1]
+        computer2 = match[2]
+        connections[computer1].append(computer2)
+        connections[computer2].append(computer1)
+    return connections
 
-computer_connections = defaultdict(lambda : [])
-for connection in connections:
-    match =  re.match(REGEX, connection)
-    computer1 = match[1]
-    computer2 = match[2]
-    computer_connections[computer1].append(computer2)
-    computer_connections[computer2].append(computer1)
+def all_computers_connected(computers):
+    for computer1, computer2 in combinations(computers, 2):
+        if computer1 not in connections[computer2]:
+            return False
+    return True
 
-print(computer_connections)
-list_of_groups = []
-for k, v in computer_connections.items():
-    for a, b in combinations(v, 2):
-        if a in computer_connections[b] and ('t' in [k[0], a[0], b[0]]):
-            computers = [k , a, b]
-            computers.sort()
-            list_of_groups.append(''.join(computers))
+def find_network_with_size(computers, network_size):
+    for computers_to_check in combinations(computers, network_size):
+        if all_computers_connected(computers_to_check):
+            return computers_to_check
 
-unique_connections = set(list_of_groups)
-print(unique_connections)
-print(len(unique_connections))
+def find_larger_network(computers, largest_network_found_size):
+    for network_size in range(len(computers), largest_network_found_size - 1, -1):
+        network = find_network_with_size(computers, network_size)
+        if network:
+            return network
+
+def find_networks_with_3_computers_and_t_computer():
+    list_of_groups = []
+    for computer1, connected_computers in connections.items():
+        if computer1[0] != 't':
+            continue
+        for computer2, computer3 in combinations(connected_computers, 2):
+            if computer2 in connections[computer3]:
+                computers = [computer1, computer2, computer3]
+                computers.sort()
+                list_of_groups.append(''.join(computers))
+    return len(set(list_of_groups))
+
+def find_largest_network():
+    largest_network_found = []
+    for computer, connected_computers in connections.items():
+        network_found = find_larger_network(connected_computers, len(largest_network_found))
+        if network_found:
+            largest_network_found = [*network_found, computer]
+    largest_network_found.sort()
+    return largest_network_found
+
+connections = find_connections_by_computer(connection_list)
+print(find_networks_with_3_computers_and_t_computer())
+print(','.join(find_largest_network()))
+print("--- %s seconds ---" % (time.time() - start_time))
