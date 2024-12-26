ed54335da9caa7ce0da1f0acce174763d0be1fb6
David Morton <55235981+davidmorton0@users.noreply.github.com>
2024-12-26 17:54:12 +0000

    day6 part 2

--- day3/p1.py
+++ day3/p1.py
@@ -1,26 +1,36 @@
 #!/usr/bin/env python
 import time
 import re
+from itertools import pairwise
 
 start_time = time.time()
 
-INPUT = 1
+INPUT = 2
 FILENAME = ['example_1.txt', 'example_2.txt', 'input_h.txt', 'input_w.txt'][INPUT]
 REGEX_1 = r'mul\(\d+,\d+\)'
 REGEX_2 = r'mul\((\d+),(\d+)\)'
-REGEX_3 = r'^(.*)don\'t\(\).*do\(\)(.*)(don\'t\(\).*)?'
+REGEX_3 = r'(do\(\)|don\'t\(\))'
+
 
 with open(FILENAME, 'r') as file:
     input = file.read()
 
-def process_instruction(instruction):
+def calculate_instruction_total(instruction):
     numbers = re.match(REGEX_2, instruction)
     return int(numbers[1]) * int(numbers[2])
 
-instructions1 = re.findall(REGEX_1, input)
-total = sum([process_instruction(instruction) for instruction in instructions1])
+def calculate_input_total(input):
+    instructions = re.findall(REGEX_1, input)
+    return sum([calculate_instruction_total(instruction) for instruction in instructions])
 
-instructions2 = re.match(REGEX_3, input)
-print(instructions2.groups())
+instructions_total = calculate_input_total(input)
+print(instructions_total)
+
+split_input = re.split(REGEX_3, input)
+total = 0
+for part1, part2 in pairwise(['do()', *split_input]):
+    if part1 == 'do()':
+        total += calculate_input_total(part2)
+print(total)
 
 print("--- %s seconds ---" % (time.time() - start_time))
