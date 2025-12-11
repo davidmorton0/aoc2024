ba785a700d13c3cccd272d21c49d35908a52b2a0
David Morton <55235981+davidmorton0@users.noreply.github.com>
2025-12-11 08:31:52 +0000

    day 10 in progr

--- 2025/day10/solve.py
+++ 2025/day10/solve.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 import time
+from itertools import product
+from sympy import symbols, Eq, solve
+DIGITS = "abcdefghijklmn"
 
 class Solve:
     def __init__(self):
@@ -68,36 +71,46 @@ class Solve:
 
     def solve_b(self, filename):
         self.load_input(filename)
+        total = 0
+        for machine in self.machines:
+            count = self.calculate_presses_b(machine)
+            # print(count)
+            # total += count
+        print(total)
     
-    def calculate_presses_b(self, machine):
-        final_lights = machine["lights"]
+    def calculate_presses_b(self, machine):        
+        joltage = machine["joltage"]
         buttons = machine["buttons"]
-        states = self.generate_state(len(buttons))
-        lowest = None
-        for state in states:
-            lights_state = len(final_lights) * "."
-            for n in range(0, len(state)):
-                if state[n] == "1":
-                    lights_state = self.press_button(buttons[n], lights_state)
-            if lights_state == final_lights:
-                button_presses = sum([int(i) for i in list(state)])
-                if lowest is None or button_presses < lowest:
-                    lowest = button_presses
-        return lowest
-    
+        a, b, c, d, e, f = symbols("a b c d e f", integer=True)
+        eq1 = Eq(d + f, 3)
+        eq2 = Eq(b + f, 5)
+        eq3 = Eq(c + d + e, 4)
+        eq4 = Eq(a + b + d, 7)
+        solution = solve([eq1,eq2, eq3, eq4], (a,b,c,d,e,f))
+        print(solution)
+        
+        # states = self.generate_state_b(len(buttons), max(joltage))
+        # lowest = None
+        # for state in states:
+        #     print(state)
+        #     # for button in buttons:
+        #     #     if state[n] == "1":
+        #     #         lights_state = self.press_button(buttons[n], lights_state)
+        #     # if lights_state == joltage:
+        #     #     button_presses = sum([int(i) for i in list(state)])
+        #     #     if lowest is None or button_presses < lowest:
+        #     #         lowest = button_presses
+        # return lowest
+
     def generate_state_b(self, length, max_value):
         states = list(product(range(0, max_value + 1), repeat=length))
         for i in range(0, length):
             states.append(str(bin(i))[2:].rjust(length, '0'))
         return states
 
-
-
-
-
 start_time = time.time()
 # Solve().solve_a('example.txt')
-# Solve().solve_a('input.txt')
+# Solve().solve_a('input_2.txt')
 print("\n")
 Solve().solve_b('example.txt')
 # Solve().solve_b('input.txt')
